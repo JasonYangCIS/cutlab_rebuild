d9aaeb664e6c3392925201db917b6d6194536dc5
JasonYangCIS <jasonYangCIS@gmail.com>
2017-02-19 23:11:04 -0800

    finish blog sidebar on homepage

--- wp-content/themes/cutlab/sidebar.php
+++ wp-content/themes/cutlab/sidebar.php
@@ -8,10 +8,20 @@
  */
 ?>
 
-	<aside class="sidebar">
-
+<aside class="sidebar">
 	<?php
 	if ( is_active_sidebar( 'sidebar' ) ) : dynamic_sidebar( 'sidebar' ); ?>
 				
 	<?php endif; ?>
-	</aside>
+
+	<?php query_posts( array('post_type' => 'post', 'posts_per_page' => -1, 'orderby' => 'menu_order title', 'order' => 'DESC')); ?>
+	<ul class="blog-feed-container">
+		<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+			<li>
+				<a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a>
+				<span class="post-date">Published on:<?php echo get_the_date(); ?></span>
+			</li>
+		<?php endwhile; endif; ?>
+	</ul>
+	<?php wp_reset_query(); ?>
+</aside>
