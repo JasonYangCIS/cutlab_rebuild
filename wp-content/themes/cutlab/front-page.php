3dc7ec587079f05b4402d57462544893c30a0874
JasonYangCIS <jasonYangCIS@gmail.com>
2017-02-19 17:58:50 -0800

    homepage, finish staff feed, menu, and working on instagram

--- wp-content/themes/cutlab/front-page.php
+++ wp-content/themes/cutlab/front-page.php
@@ -26,14 +26,46 @@
 
       <section id="team">
         <h2 class="section-title"><span><?php echo get_field('section_title_2');?></span></h2>
+        <article>
+          <?php query_posts( array('post_type' => 'employees', 'posts_per_page' => -1, 'orderby' => 'menu_order title', 'order' => 'ASC')); ?>
+          <ul class="projects-container">
+            <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+              <li>
+                <span class="staff-name"><a href="<?php echo the_permalink();?>"><?php the_title(); ?></a></span>
+                <span class="position"><?php echo get_field('position'); ?></span>
+              </li>
+            <?php endwhile; endif; ?>
+            <?php wp_reset_query(); ?>
+          </ul>
+        </article>
       </section>
 
       <section id="menu">
         <h2 class="section-title"><span><?php echo get_field('section_title_3');?></span></h2>
+        <?php 
+        $image = get_field('banner_image');
+        if( !empty($image) ) {
+          $imageLarge = $image['sizes']['medium_large'];
+        }
+        ?>
+        <div class="menu-banner">
+          <img title="<?php the_title(); ?>" src="<?php echo $imageLarge ?>" />
+        </div>
+        <?php for($i = 1; $i <= 3; $i++){ ?>
+          <div class="menu-column">
+            <?php echo get_field('column_' . $i); ?>
+          </div>
+        <?php }; //end for loop ?>
       </section>
 
       <section id="blog">
         <h2 class="section-title"><span><?php echo get_field('section_title_4');?></span></h2>
+        <div class="container-left">
+          <?php echo get_field('content_instagram'); ?>
+        </div>
+        <div class="container-right">
+          <?php echo get_sidebar('blog'); ?>
+        </div>
       </section>
 
       <section id="contact">
